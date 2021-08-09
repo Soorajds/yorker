bd0f8811bedc768681802c1371522ccdd6d59ccc
Soorajds <79838329+Soorajds@users.noreply.github.com>
2021-08-09 15:50:45 +0530

    seeder added

--- api/database/seeders/PlayerSeeder.php
+++ api/database/seeders/PlayerSeeder.php
@@ -4,17 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Player;
+
+class PlayerSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            PlayerSeeder::class,
+        Player::insert([
+            'name' => 'Sooraj',
         ]);
     }
 }
