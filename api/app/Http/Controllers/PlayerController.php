50383c7d566afbe765a09306fedbe4c5d786a053
Soorajds <79838329+Soorajds@users.noreply.github.com>
2021-08-29 12:16:45 +0530

    session check

--- api/app/Http/Controllers/PlayerController.php
+++ api/app/Http/Controllers/PlayerController.php
@@ -9,6 +9,8 @@ class PlayerController extends Controller
 {
    public function list(){
       $Player = Player::all();
+      session(['player_id' => 10]);
+      $value=session('player_id');
       return response()->json(['data' => $Player],200);
    }
 
@@ -16,6 +18,8 @@ class PlayerController extends Controller
     $Player = Player::join('stats','stats.player_id','players.id')
                     ->where('players.id','=',$playerId)
                     ->first();
+                    $value=session('player_id');
+                    $Player["sesion_check"]=$value;
     return response()->json(['data' => $Player],200);
  }
 }
